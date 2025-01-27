9aa1daada3c12703fd8999cd88ff7081d352c096
Ayan Shaikh <ayanms7861@gmail.com>
2025-01-27 20:09:27 +0530

    Project Done

--- app/layout.js
+++ app/layout.js
@@ -27,7 +27,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] `}
